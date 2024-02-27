4b7f8bb8e6c92f6cc98b51f1ea9b68c77c346bc1
S-Stone1263 <158242447+S-Stone1263@users.noreply.github.com>
2024-02-28 09:50:53 +1030

    Constructors and destructors implemented

---
+++ Assessment 1/Assessment 1/String.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <iostream>
+class String
+{
+private:
+
+	//declaring a character pointer
+	char* m_string;
+
+
+public:
+	String();
+	String(const char* c);
+	String(String& st);
+	~String();
+
+	/*int Length();
+	bool EqualTo(const char* c);
+	bool EqualTo(String* st);
+	void Append(const char* c);
+	void Append(String* st);
+	void Prepend(const char* c);
+	void Prepend(String* st);*/
+
+
+
+
+
+
+
+};
+
---
+++ Assessment 1/Assessment 1/main.cpp
@@ -0,0 +1,11 @@
+#include "String.h"
+#include <iostream>
+
+int main()
+{
+	String string1("Yo World");
+
+	String string2(string1);
+	int i = 2;
+
+}
