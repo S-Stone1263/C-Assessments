5a8d59cb03623e5d4a87e252bd0b17584491722c
S-Stone1263 <158242447+S-Stone1263@users.noreply.github.com>
2024-03-20 10:19:26 +1030

    Comparison between expected and actual outcomes finished

--- Assessment 3/Assessment 3/WriteToFile.cpp
+++ Assessment 3/Assessment 3/WriteToFile.cpp
@@ -1,3 +1,4 @@
+
 #include "String.h"
 #include <iostream>
 #include <fstream>
@@ -23,14 +24,14 @@ int main()
 
 	//std::fstream testLog;
 	//testLog.open("TestLog.txt", std::ios::app);
-	String string1("Hello World");
+	String string1("Hello");
 	String string2(string1);
-	char string3[6] = "Howdy";
-	String string4 = "Hello Galaxy";
-	String string5 = "a";
-	String string6 = "b";
+	String string3("Hello Galaxy");
+	String string4;
+	String string5("a");
+	String string6("b");
 
-	int expectedLength = 11;
+	int expectedLength = 5;
 	char expectedChar = 'H';
 	bool expectedEqual = true;
 	char expectedAppend[8] = "Hello a";
@@ -38,24 +39,203 @@ int main()
 	char expectedCStr[6] = "Hello";
 	char expectedToLower[6] = "hello";
 	char expectedToUpper[6] = "HELLO";
-	int expectedFind = 0;
+	int expectedFind = 2;
 	int expectedFind2 = 3;
 	char expectedFind3[8] = "Herrrro";
-	char expectedRead[] = "sean";
+	char expectedRead[] = "sean ";
 	char expectedWrite[] = "yes";
-	bool expectedEqualituy = true;
+	bool expectedEquality = true;
 	char expectedSubscript = 'H';
 	char expectedAssign[13] = "Hello Galaxy";
 	bool expectedLessThan = true;
 
+	//length
+	int realLength = string1.Length();
+	if (expectedLength == realLength)
+	{
+		std::cout << "success" << std::endl;
+	}
+	else
+	{
+		std::cout << "fail" << std::endl;
+	}
+
+	//Character at
+	char realChar = string1.CharacterAt(0);
+	if (expectedChar == realChar)
+	{
+		std::cout << "success" << std::endl;
+	}
+	else
+	{
+		std::cout << "fail" << std::endl;
+	}
+
+	//Equal to
+	bool realEqual = string1.EqualTo(string2);
+	if (expectedEqual == realEqual)
+	{
+		std::cout << "success" << std::endl;
+	}
+	else
+	{
+		std::cout << "fail" << std::endl;
+	}
+
+
+	//append
+	if (strcmp(string1.Append(string5), expectedAppend) == 0)
+	{
+		std::cout << "success" << std::endl;
+	}
+	else
+	{
+		std::cout << "fail" << std::endl;
+	}
+
+	//prepend
+	if (strcmp(string1.Prepend(string5), expectedPrepend) == 0)
+	{
+		std::cout << "success" << std::endl;
+	}
+	else
+	{
+		std::cout << "fail" << std::endl;
+	}
+
+	//CStr
+	if (strcmp(string1.Cstr(), expectedCStr) == 0)
+	{
+		std::cout << "success" << std::endl;
+	}
+	else
+	{
+		std::cout << "fail" << std::endl;
+	}
+
+	//to lower
+	if (strcmp(string1.ToLower(), expectedToLower) == 0)
+	{
+		std::cout << "success" << std::endl;
+	}
+	else
+	{
+		std::cout << "fail" << std::endl;
+	}
+
+	//to upper
+	if (strcmp(string1.ToUpper(), expectedToUpper) == 0)
+	{
+		std::cout << "success" << std::endl;
+	}
+	else
+	{
+		std::cout << "fail" << std::endl;
+	}
+
+	//find string
+	if (expectedFind == string1.Find("l")[0])
+	{
+		std::cout << "success" << std::endl;
+	}
+	else
+	{
+		std::cout << "fail" << std::endl;
+	}
 
 
 
+	//find from index
+	if (expectedFind2 == string1.Find("l", 3)[0])
+	{
+		std::cout << "success" << std::endl;
+	}
+	else
+	{
+		std::cout << "fail" << std::endl;
+	}
+
+	//replace
+	if (strcmp(string1.Find("l", "rr"), expectedFind3) == 0)
+	{
+		std::cout << "success" << std::endl;
+	}
+	else
+	{
+		std::cout << "fail" << std::endl;
+	}
+
+	//read from console
+	std::cout << "Enter 1 for number of words and sean for your word" << std::endl;
+	if (strcmp(string4.ReadFromConsole(), expectedRead) == 0)
+	{
+		std::cout << "success" << std::endl;
+	}
+	else
+	{
+		std::cout << "fail" << std::endl;
+	}
+
+	//write to console
+	string4.WriteToConsole();
+	char userResult[4];
+	std::cout << "Does the console output above read sean? (if so input yes, if not input no)" << std::endl;
+	std::cin >> userResult;
+
+	if (strcmp(userResult, expectedWrite) == 0)
+	{
+		std::cout << "success" << std::endl;
+	}
+	else
+	{
+		std::cout << "fail" << std::endl;
+	}
 
 
+	//equality operator
+	bool equalityResult = string1 == string2;
+	if (equalityResult == expectedEquality)
+	{
+		std::cout << "success" << std::endl;
+	}
+	else
+	{
+		std::cout << "fail" << std::endl;
+	}
 
+	//subscript
+	if (string1[0] == 'H')
+	{
+		std::cout << "success" << std::endl;
+	}
+	else
+	{
+		std::cout << "fail" << std::endl;
+	}
 
-		return 0;
+	//assign
+	string1 = string3;
+	if (strcmp(string1.Cstr(), expectedAssign) == 0)
+	{
+		std::cout << "success" << std::endl;
+	}
+	else
+	{
+		std::cout << "fail" << std::endl;
+	}
+
+	//Less than
+	bool lessThanResult = string5 < string6;
+	if (lessThanResult == expectedLessThan)
+	{
+		std::cout << "success" << std::endl;
+	}
+	else
+	{
+		std::cout << "fail" << std::endl;
+	}
+
+	return 0;
 
 
 
