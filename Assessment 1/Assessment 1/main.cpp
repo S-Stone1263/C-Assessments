9783ade4c22700d5837281b216fe7301f34333c8
S-Stone1263 <158242447+S-Stone1263@users.noreply.github.com>
2024-03-20 09:42:56 +1030

    Adding function testing

--- Assessment 1/Assessment 1/main.cpp
+++ Assessment 1/Assessment 1/main.cpp
@@ -1,16 +1,59 @@
 #include "String.h"
 #include <iostream>
 
+void clearAndIgnore();
+void lengthOutput();
+void characterOutput();
+void equalTo();
+void seeString();
+void appendOutput();
+void prependOutput();
+
 
 int main()
 {
-	String string1("Hello");
-	String string2("Hello Galaxy");
+	String string1;
+	String string2;
 
-	std::cout << string1.Find("Hell", "COD") << std::endl;
+	std::cout << "--- LENGTH FUNCTION ---" << std::endl;
+	std::cout << "The length of this string is " << string1.Length() << std::endl;
+
+	std::cout << "--- CHARACTER AT FUNCTION ---" << std::endl;
+
+	int numberOfWords;
+	int inputCheck;
+	bool flag = true;
+
+	std::cout << "The character at that point is " << string1.CharacterAt(5) << std::endl;
 
 	return 0;
 
 
 
+}
+
+void lengthOutput()
+{
+
+}
+
+void characterOutput()
+{
+
+}
+
+void equalTo()
+{
+}
+
+void seeString()
+{
+}
+
+void appendOutput()
+{
+}
+
+void prependOutput()
+{
 }
